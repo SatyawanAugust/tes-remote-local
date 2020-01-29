fa303827d74e94465f3840e173be9e243f0cdd3f
Satyawan Augustinus <dev.satyawan@gmail.com>
2020-01-29 16:15:28 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h1>Hello PHP</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ducimus quidem? Vero doloribus itaque dicta fugiat repudiandae minima officia, accusamus doloremque omnis perspiciatis repellendus numquam ipsa aut suscipit laborum provident!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+        <li>Tiga</li>
+    </ul>
 </body>
 
 </html>
